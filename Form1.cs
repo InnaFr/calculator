2bc2439d1d9e8d2b65b436c91889bc54dc35c46a
InnaFr <fr.monstr@gmail.com>
2018-06-27 17:48:48 +0800

    Commit

--- Form1.cs
+++ Form1.cs
@@ -56,17 +56,15 @@ namespace Calculator
 
         public void substract(long arg) {
             //TODO: add test
-            currentResult -= arg;
+            currentResult -= arg+1;
         }
 
         public void multiply(long arg) {
-            //TODO: add test
-            //TODO: implement
+            currentResult *= arg;
         }
 
         public void divide(long arg) {
-            //TODO: add test
-            //TODO: implement
+            currentResult /= arg;
         }
     }
 }
